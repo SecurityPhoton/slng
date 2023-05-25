e4ff14d5b7bffef413f1ce007b94afc7582dde3a
Victor <pontarr@gmail.com>
2023-05-25 14:58:24 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,10 @@ echo "Update and setup..."
 apt update
 apt install mc syslog-ng -y
 echo "Copy config file ..."
+
 cp ./system-log.conf /etc/syslog-ng/conf.d/system-log.conf
+echo "Creating dir for logs ..."
+mkdir /var/log/system-log
 echo "Restarting service ..."
 service syslog-ng status
 service syslog-ng restart
@@ -14,7 +17,7 @@ netstat -nlp | grep 514
 
 echo " _____  _    _   ____    _        _____    ___    "
 echo "/  ___|| |  | | / __ |  | |      /  /\ \  / /\_\  "
-echo "\ \`--. \ \/ / | |___   | |      | |  | || |      "
-echo " \`--. \ | |   \___  \  | |      | |  | || |  __  "
+echo "\ \`--.  \ \/ / | |___   | |      | |  | || |      "
+echo " \`--. \  | |   \___  \  | |      | |  | || |  __  "
 echo "/\__/ /  | |   /\__/ /  | |_____ | \__/ ||  \_/ | "
 echo "\____/   |_|   \____/   |______|  \____/  \____/  "
