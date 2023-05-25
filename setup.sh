f6d23a3fee3cacce1fca350e5e4483186573bc25
Victor <pontarr@gmail.com>
2023-05-25 14:50:14 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Update and setup..."
+apt update
+apt install mc syslog-ng -y
+echo "Copy config file ..."
+cp ./system-log.conf /etc/syslog-ng/conf.d/system-log.conf
+echo "Restarting service ..."
+service syslog-ng status
+service syslog-ng restart
+service syslog-ng status
+echo "Checking networking ..."
+netstat -nlp | grep 514
+
+echo " _____  _    _   ____    _        _____    ___    "
+echo "/  ___|| |  | | / __ |  | |      /  /\ \  / /\_\  "
+echo "\ \`--. \ \/ / | |___   | |      | |  | || |      "
+echo " \`--. \ | |   \___  \  | |      | |  | || |  __  "
+echo "/\__/ /  | |   /\__/ /  | |_____ | \__/ ||  \_/ | "
+echo "\____/   |_|   \____/   |______|  \____/  \____/  "
