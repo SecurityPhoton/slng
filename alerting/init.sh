d69c6a4affe6bbe11631261d3b7577e614b46bb2
Victor <pontarr@gmail.com>
2023-08-18 15:18:12 +0300

    Update init.sh

--- alerting/init.sh
+++ alerting/init.sh
@@ -4,4 +4,9 @@ mkdir /etc/syslog.pipes
 mknod /etc/syslog.pipes/criticalMessages p
 chmod 600 /etc/syslog.pipes/criticalMessages
 mv syslogMailer /usr/bin/syslogMailer
- 
+
+apt install mailutils postfix -y
+
+apt remove exim4
+apt-get autoremove
+
